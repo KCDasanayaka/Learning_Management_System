2b90a0551543165ec5579ee8708d3bd745349924
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-12-22 12:44:26 +0530

    change file structure and create header component

--- LMS_frontEnd/tailwind.config.js
+++ LMS_frontEnd/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{jsx}', // Adjust based on your folder structure
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
-
 
