64127d46227c631811aaaadb9c95c622c3892dc3
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-12-22 17:10:30 +0530

    Created body component

--- LMS_frontEnd/src/pages/Home.jsx
+++ LMS_frontEnd/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import 'react'
 import HeroImageCarousel from '../components/HeroCarousel'
+import Body from '../components/Body'
 
 function Home() {
   return (
     <div className="font-inter">
       <HeroImageCarousel />
+      <Body />
     </div>
   )
 }
