9f2ca17a481680cf75a13f12a24fadd4eac2d111
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2025-02-19 20:24:28 +0530

    change ui of the admin

--- Admin_Frontend/tailwind.config.js
+++ Admin_Frontend/tailwind.config.js
@@ -1,11 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        kumbh: ['"Kumbh Sans"', 'sans-serif'],
+      },
+      backgroundColor: {
+        'red-custom': '#4E0A0A',
+        'yellow-custom': '#F9C524',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      colors: {
+        'yellow-custom': '#F9C524',
+        'red-custom': '#4E0A0A',
+      },
+      fontWeight: {
+        'custom-custom': '550',
+      },
+      width: {
+        'custom': '80%',
+      },
+      height: {
+        'custom': '3.5px',
+      },
+      fontSize: {
+        'xl-custom': '24px',
+      }
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.font-optical': {
+          'font-optical-sizing': 'auto',
+        },
+        '.font-yopq-300': {
+          'font-variation-settings': '"YOPQ" 300',
+        },
+        '.list-point-image': {
+          'list-style-image': 'url(./assets/Point.png)',
+          'background-size': '2px 2px',  // Set the width and height of the bullet image here
+          'background-repeat': 'no-repeat',
+          'background-position': 'left center',
+        },
+        // Add more custom utilities here as needed
+      });
+    },
+  ],
+};
