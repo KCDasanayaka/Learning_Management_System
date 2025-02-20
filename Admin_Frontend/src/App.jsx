79405832854895749177871392f8c2f35755bf3d
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2025-02-20 23:30:26 +0530

    changed the ui design event creation

--- Admin_Frontend/src/App.jsx
+++ Admin_Frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateNotice from './pages/CreateNotice';
 import Header from './components/Header';
+import CreateEventPage from './pages/CreateEventPage';
+import GalleryPage from './pages/GalleryPage';
 
 
 const App = () => {
@@ -11,6 +13,8 @@ const App = () => {
       <Header/>
       <Routes>
         <Route path="/create-notice" element={<CreateNotice />} />
+        <Route path="/create-gallery" element={<CreateEventPage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
       </Routes>
     </Router>
   );
