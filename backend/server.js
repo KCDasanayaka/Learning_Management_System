c8423521f9c3510562ce77523ff970b8f311ff74
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2025-02-20 22:48:55 +0530

    implemented backend of the gallery

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const noticeRoutes = require('./src/routes/noticeRoutes');
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./src/config/db");
+
+const noticeRoutes = require("./src/routes/noticeRoutes");
+const eventRoutes = require("./src/routes/eventRoutes");
 
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Connect to mongooDB
+connectDB();
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api', noticeRoutes);
+app.use("/api", noticeRoutes);
+app.use("/api/events", eventRoutes);
 
-// Connect to mongooDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB Successfully");
-  })
-  .catch((error) => console.error(error));
+// Error handling middleware
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message });
+});
 
 // Check the PORT
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-  });
+  console.log(`Server is running on port: ${PORT}`);
+});
