b39c0df7bf809bfbfe23457ab3e432cf6069d426
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2025-02-23 00:34:22 +0530

    implemented notifications into login page

--- Admin_Frontend/src/components/LogoutButton.jsx
+++ Admin_Frontend/src/components/LogoutButton.jsx
@@ -14,7 +14,7 @@ const LogoutButton = ({ setIsLoggedIn }) => { // Accept setIsLoggedIn as a prop
   return (
     <button
       onClick={handleLogout}
-      className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition duration-300"
+      className="bg-red-900 text-white px-4 py-2 rounded hover:bg-yellow-custom hover:text-red-custom transition duration-300"
     >
       Logout
     </button>
